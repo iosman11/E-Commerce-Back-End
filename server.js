9328e5e5e4daea82a702f10af406b47d2d523e39
idil osman <idil_abukar@live.se>
2022-09-11 18:08:14 +0100

    source schema done

--- server.js
+++ server.js
@@ -1,22 +1,17 @@
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
-const connection = require('./config/connection');
+
+const sequelize = require("./config/connection")
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(routes);
-// sync sequelize models to the database, then turn on the server
-const init = async () => {
-  try {
-    await connection.sync({ force: false });
-    console.log(`[INFO]: DB connection successful`);
-    app.listen(PORT, () =>
-      console.log(`Server is running on http://localhost:${PORT}`)
-    );
-  } catch (error) {
-    console.error(`[ERROR]: DB connection failed | ${error.message}`);
-  }
-};
-init();
+
+sequelize.sync().then(() => {
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+})})

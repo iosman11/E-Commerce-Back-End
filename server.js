3467a893ee2361d82b164acfe846a832542aa81f
idil osman <idil_abukar@live.se>
2022-09-11 18:05:52 +0100

    trying to fix code and sql error

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 const express = require('express');
-const routes = require('./routes ');
-const sequelize = require("./config/connection");
-
-
+const routes = require('./routes');
+// import sequelize connection
+const connection = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(routes);
-
 // sync sequelize models to the database, then turn on the server
-sequelize.sync().then(() => {
-  app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-  });
-});
+const init = async () => {
+  try {
+    await connection.sync({ force: false });
+    console.log(`[INFO]: DB connection successful`);
+    app.listen(PORT, () =>
+      console.log(`Server is running on http://localhost:${PORT}`)
+    );
+  } catch (error) {
+    console.error(`[ERROR]: DB connection failed | ${error.message}`);
+  }
+};
+init();
